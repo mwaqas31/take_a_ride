de9fd185575c7f0344188df47b15145a9b4581e8
Waqas Mubarik <mwaqas0031@gmail.com>
2014-02-05 22:21:50 +0500

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TakeARide::Application.config.secret_key_base = 'f9bf09e0b6cf15f9149a2f24f111b339c4c1dce4b0c17e17f7bb17615f65e09df1dd0f77ef7c1c367f1d152339953684070baed1ea99819e9073053315d4b7c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TakeARide::Application.config.session_store :cookie_store, key: '_take_a_ride_session'
